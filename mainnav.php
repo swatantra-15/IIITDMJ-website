fbcb3e1519510884a90378a267f3aed6518d8ca4
Yash Goel <89704419+GoelYash1@users.noreply.github.com>
2021-09-14 14:33:13 +0530

    changes made in style.css and mainnav.php

--- mainnav.php
+++ mainnav.php
@@ -8,10 +8,39 @@
     </div>
     <div class="Navigation-bar">
         <ul>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">DEPARTMENTS</a></li>
-            <li><a href="#">FACULTY</a></li>
-            <li><a href="#">DEANS</a></li>
+            <li>
+                <a href="#">HOME</a>
+                <div class="dropdown">
+                    <a href="about.php">About</a>
+                    <a href="#">General Administraion</a>
+                </div>
+            </li>
+            <li>
+                <a href="#">DEPARTMENTS</a>
+                <div class="dropdown">
+                    <a href="#">Computer Science & Engineering(CSE)</a>
+                    <a href="#">Electronics & Communication Engineering(ECE)</a>
+                    <a href="#">Design(DES)</a>
+                    <a href="#">Mechanical Engineering(ME)</a>
+                    <a href="#">Natural Sciences(NS)</a>
+                </div>
+            </li>
+            <li>
+                <a href="#">PEOPLE</a>
+                <div class="dropdown">
+                    <a href="#">Faculty</a>
+                    <a href="#">Deans</a>
+                    <a href="#">Research Staff</a>
+                </div>
+            </li>
+            <li>
+                <a href="#">STUDENTS</a>
+                <div class="dropdown">
+                    <a href="#">Gymkhana</a>
+                    <a href="#">Hostels</a>
+                    <a href="#">Activities</a>
+                </div>
+            </li>
             <li><a href="#">CONTACT</a></li>
         </ul>
     </div>
