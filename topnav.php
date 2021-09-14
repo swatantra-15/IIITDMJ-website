87691a9c626004fe060a0ba9d3ae9d02992b0fca
Yash Goel <89704419+GoelYash1@users.noreply.github.com>
2021-09-14 16:25:23 +0530

    Add files via upload

---
+++ topnav.php
@@ -0,0 +1,14 @@
+<header>
+    <nav class="top-navbar">
+        <ul>
+            
+            <li><a href="#">PLACEMENT</a></li>
+            <li><a href="#">ORDINANCES</a></li>
+            <li><a href="#">JOB</a></li>
+            <li><a href="#">TENDER</a></li>
+            <li><a href="#">ALUMINI</a></li>
+            <li><button type="submit">SEARCH<input type="text" style="float: right; border-radius: 14px; text-align: center;"></button></li>
+            <li><a href="#" style="float: right;padding-right: 60px;" class="login">LOGIN</a></li>
+        </ul>
+    </nav>
+</header>
