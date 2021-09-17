1b9e6ec57f60928e025061c0fb5f4ab66705a7ec
Yash Goel <89704419+GoelYash1@users.noreply.github.com>
2021-09-17 11:16:20 +0530

    spelling mistake

--- topnav.php
+++ topnav.php
@@ -5,7 +5,7 @@
             <li><a href="Downloads/Ordinances.pdf" target="_blank">ORDINANCES</a></li>
             <li><a href="#">JOB</a></li>
             <li><a href="#">TENDER</a></li>
-            <li><a href="#">ALUMINI</a></li>
+            <li><a href="#">ALUMNI</a></li>
             <li><button type="submit">SEARCH<input type="text" style="float: right; border-radius: 14px; text-align: center;"></button></li>
             <li><a href="login.html" style="float: right;padding-right: 60px;" class="login">LOGIN</a></li>
         </ul>
