8b716a359cd14db4bed68655af4d2462188136e6
Yash Goel <ygoel172001@gmail.com>
2021-10-24 14:46:56 -0700

    acad_calendar.php linked with database

--- Acad_Calendar.php
+++ Acad_Calendar.php
@@ -9,9 +9,39 @@
     <link rel="stylesheet" href="stylesheets/style.css">
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <link rel="stylesheet" href="stylesheets/about.css">
-    <link rel="stylesheet" href="stylesheets/carousel.css">
     <link rel="stylesheet" href="stylesheets/academic.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        #block {
+            height: 88%;
+            width: 95%;
+            margin: auto;
+            margin-top: 3%;
+            margin-bottom: 2.5%;
+            background-color: #ebe0d2;
+            box-shadow: 10px 10px grey;
+        }
+
+        .container h1 {
+            margin-bottom: 1.2rem;
+            color: rgb(43, 43, 153);
+            text-shadow: 1px 1px 2px black;
+        }
+
+        .table-bordered>tbody>tr>td,
+        .table-bordered>tbody>tr>th,
+        .table-bordered>tfoot>tr>td,
+        .table-bordered>tfoot>tr>th,
+        .table-bordered>thead>tr>td,
+        .table-bordered>thead>tr>th {
+            border: 1px solid #555;
+        }
+
+        .table-bordered {
+            border: 3px solid #555;
+            width: 95%;
+        }
+    </style>
 </head>
 
 <body>
@@ -23,336 +53,65 @@
             </h1>
         </div>
     </div>
-    <div class="features">
-        <section class="featurescontent">
-            <section class="child" style="min-width: 70%; margin:auto">
-                <i class="fa fa-calendar" style="float: left;"></i>
-                <h2>Academic Calendar for Session 2021-22</h2><br>
-                <table border="2px" style="box-shadow: 0 5px 5px 5px ; border-color:beige">
-                    <tr>
-                        <td colspan="3" align="center" style="font-weight:bold;text-decoration:underline;color:#FF0000;font-size:14px;">Semester I, 2021-22</td>
-                    </tr>
-                    <tr bgcolor="#CCCCCC" style="font-weight:bold;">
-                        <td>Academic Calendar Semester I 2020-21</td>
-                        <td>Old UG+PG</td>
-                        <td>New PG M.Tech. +MDes+ Ph.D. (2021 Batch)</td>
-                    </tr>
-                    <tr>
-                        <td>Events</td>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Pre-Registration</td>
-                        <td>June 28-July 10, 2021(Mon- Sat)</td>
-                        <td>--</td>
-                    </tr>
-                    <tr>
-                        <td>Online Registration/ Physical Reporting at the Institute [only if applicable]</td>
-                        <td>July 16, 2021 (Fri)</td>
-                        <td>August 27, 2021 (Friday)</td>
-                    </tr>
-                    <tr>
-                        <td>Late Physical reporting</td>
-                        <td>July 23, 2021 (Fri)</td>
-                        <td>--</td>
-                    </tr>
-                    <tr>
-                        <td>Orientation of new students</td>
-                        <td>-</td>
-                        <td>August 27-28, 2021 (Fr - Sat)</td>
-                    </tr>
-                    <tr>
-                        <td>Commencement of Classes</td>
-                        <td>July 19, 2021 (Mon)</td>
-                        <td>Aug 31, 2021 (Tue)</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date for Adding of courses</td>
-                        <td>July 26, 2021 (Mon)</td>
-                        <td>Sep 06, 2021 (Mon)</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date for dropping of courses</td>
-                        <td>Aug 20, 2021 (Fri)</td>
-                        <td>Oct 04, 2021 (Mon)</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date for Document Submission (New Admissions)</td>
-                        <td>-</td>
-                        <td>Oct 08, 2021 (Fri)</td>
-                    </tr>
-                    <tr>
-                        <td>Mid Semester Exams&nbsp;</td>
-                        <td>Sep 06-11, 2021 (Mon-Sat)</td>
-                        <td>Oct 18-24, 2021 (Mon-Sun)</td>
-                    </tr>
-                    <tr>
-                        <td>Mid-Semester Recess</td>
-                        <td>Oct 09-17, 2021 (Sat- Sun)</td>
-                        <td>--</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date for showing mid semester answer sheets to students and display of compiled marks upto mid semester</td>
-                        <td>Oct 08, 2021 (Fri)</td>
-                        <td>Nov 08, 2021 (Mon)</td>
-                    </tr>
-                    <tr>
-                        <td>Classes end</td>
-                        <td>Nov 03, 2021 (Wed)</td>
-                        <td>Dec 10, 2021 (Fri)</td>
-                    </tr>
-                    <tr>
-                        <td>End Semester Exams</td>
-                        <td>Nov 08-13, 2021 (Mon-Sat)</td>
-                        <td>Dec 13-17, 2021 (Mon-Fri)</td>
-                    </tr>
-                    <tr>
-                        <td>PG Presentations (Graduate/Research/Progress Seminars)</td>
-                        <td>Oct 18 - Nov 17, 2021 (Mon-Wed)</td>
-                        <td>--</td>
-                    </tr>
-                    <tr>
-                        <td>Last date for showing of&nbsp; compiled marks (of the full semester)</td>
-                        <td>Nov 18, 2021 (Thu)</td>
-                        <td>Dec 22, 2021 (Wed)</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date of End Semester Grades Submission for courses</td>
-                        <td>Nov 20, 2021 (Sat)</td>
-                        <td>Dec 23, 2021 (Thu)</td>
-                    </tr>
-                    <tr>
-                        <td>Make-up Examinations</td>
-                        <td>Nov 29 Dec 01, 2021 (Mon-Wed)</td>
-                        <td>Dec 30-31, 2021 (Thu-Fri)</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date for Grades Submission of Make-up Exams, Conversion of I Grades, Graduate/Research/Progress Seminars</td>
-                        <td>Dec 03, 2021 (Fri)</td>
-                        <td>Jan 04, 2022 (Tue)</td>
-                    </tr>
-                    <tr>
-                        <td>Submission of grade moderation report from discipline</td>
-                        <td>Dec 10, 2021 (Fri)</td>
-                        <td>Dec 27, 2021 (Mon)</td>
-                    </tr>
-                    <tr>
-                        <td>Submission of moderation report by APCS</td>
-                        <td>Dec 15, 2021 (Wed)</td>
-                        <td>Dec 30, 2021 (Thu)</td>
-                    </tr>
-                    <tr>
-                        <td>Last date for Authentication of Grades</td>
-                        <td>Jan 17, 2022 (Mon)</td>
-                        <td>Jan 17, 2022 (Mon)</td>
-                    </tr>
-                    <tr>
-                        <td>Academic Pre-Registration for next semester</td>
-                        <td>Oct 18-22, 2021 (Mon-Fri)</td>
-                        <td>Oct 18-22, 2021 (Mon-Fri)</td>
-                    </tr>
-                    <tr>
-                        <td>Vacation for UG students</td>
-                        <td>Nov 15 - Jan 02, 2022</td>
-                        <td>--</td>
-                    </tr>
-                    <tr>
-                        <td>Vacational leave applicable for faculty</td>
-                        <td>Nov 15 - Jan 02, 2022</td>
-                        <td>--</td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td colspan="3" align="center" style="font-weight:bold;text-decoration:underline;color:#FF0000;font-size:14px;">Semester II, 2021-22</td>
-                    </tr>
-                    <tr bgcolor="#CCCCCC" style="font-weight:bold;">
-                        <td>Academic Calendar Semester I 2020-21</td>
-                        <td>Old UG+ All PG</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Events</td>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Pre-Registration</td>
-                        <td>Oct 18-22, 2021 (Mon-Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Online Registration/ Physical Reproting at the Institute</td>
-                        <td>Jan 03, 2022 (Mon)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Orientation of new students</td>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Commencement of Classes</td>
-                        <td>Jan 03, 2022 (Mon)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Late physical reporting*</td>
-                        <td>Jan 07, 2022 (Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date for Adding of courses</td>
-                        <td>Jan 10, 2022 (Mon)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date for dropping of courses</td>
-                        <td>Feb 07, 2022 (Mon)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date for Document Submission (New Admissions)</td>
-                        <td>Feb 25, 2022 (Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Mid-Semester Examinations</td>
-                        <td>Feb 21 - 26, 2022 (Mon-Sat)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Mid-Semester Recess</td>
-                        <td>Mar 12- 20, 2022 (Sat-Sun)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date for showing mid semester answer sheets to students and display of compiled marks upto mid semester</td>
-                        <td>Mar 21, 2022 (Mon)&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Classes End</td>
-                        <td>Apr 22,2022 (Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>End-Semester Examinations</td>
-                        <td>Apr 25 - 30, 2022 (Mon-Sat)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>PG Presentations (Graduate/Research/Progress Seminars)</td>
-                        <td>Apr 11-May 04, 2022 (Mon-Wed)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Last date for showing of End-Semester answer sheets to students and display of compiled marks (of the full semester)</td>
-                        <td>May 05, 2022 (Thu)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date of End Semester Grades Submission for courses</td>
-                        <td>May 06, 2022 (Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Make-up Examinations</td>
-                        <td>May 09-11, 2022 (Mon-Wed)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Last Date for Grades Submission of Make-up Exams, Conversion of I Grades, Graduate/Research/Progress Seminars</td>
-                        <td>May 13, 2022 (Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Submission of grade moderation report from discipline</td>
-                        <td>May 13, 2022 (Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Submission of moderation report by APCS</td>
-                        <td>May 18, 2022 (Wed)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Last date for Authentication of Grades</td>
-                        <td>Jun 10, 2022 (Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Academic Pre-Registration for next semester</td>
-                        <td>Apr 11-15, 2022 (Mon-Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Vacation for UG students</td>
-                        <td>May 02-July 15, 2022 (Mon-Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>Vacational leave applicable for faculty in case not enganged in teaching</td>
-                        <td>May 02-July 15, 2022 (Mon-Fri)</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr bgcolor="#CCCCCC" style="font-weight:bold;">
-                        <td colspan="3" align="center">Summer Term2021-22</td>
-                    </tr>
-                    <tr>
-                        <td>Registration</td>
-                        <td>May 02, 2022 (Mon)</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Commencement of Classes</td>
-                        <td>May 02, 2022 (Mon)</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Mid-Semester Examinations</td>
-                        <td>Jun 06-08, 2022 (Mon-Wed)</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Classes End</td>
-                        <td>July 13, 2022 (Wed)</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>End-Semester Examinations</td>
-                        <td>July 14-16, 2022 (Thu-Sat)</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Last Date of End Semester Grades Submission for courses</td>
-                        <td>July 20, 2022 (Wed)</td>
-                        <td></td>
-                    </tr>
-                </table>
-                <br>
+    <div id="block" class="container" style="padding: 0.8rem;">
+        <div class="container" style="text-align: center;">
+        <i class="fa fa-calendar" style="font-size: x-large;"></i>
+            <h1>Academic Calendar for Session 2021-22</h1>
+        </div>
+        <br>
+        <table class="table table-bordered  text-center " style="margin:auto; margin-bottom: 20px;">
+            <thead>
+                <tr style="background-color: rgba(0,0,0,0.6);color:white; font-size:larger">
+                    <th></th>
+                    <th> Event </th>
+                    <th> Old UG+PG </th>
+                    <th> New Pg M.Tech +Mdes+Ph.D(2021 Batch)</th>
+                </tr>
+                <tr>
+                    <th colspan="4" style="color: red;">Semester 1 </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $link = mysqli_connect('localhost', 'root', '', 'iiitdmj');
+                /*Check link to the mysql server*/
+                if (!$link) {
+                    die('Failed to connect to server: ');
+                }
+                /*Create query*/
+                $qry = 'SELECT * FROM acad_calendar WHERE sem=1';
 
-            </section>
-        </section>
+                /*Execute query*/
+                $result = mysqli_query($link, $qry);
+
+                /*Show the rows in the fetched result set one by one*/
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo
+                    '<tr> 
+          <td style="font-size: medium">' . $row['cal_id'] . '</td>
+          <td style="font-size: medium">' . $row['heading'] . '</td>
+          <td style="font-size: medium">' . $row['date_ug_pg'] . '</a></td>
+          <td style="font-size: medium">' . $row['date_else'] . '</a></td>
+          </tr>';
+                }
+                echo '<tr>
+                <th colspan="4" style="color: red;">Semester 2 </th>
+            </tr>';
+                $qry2= 'SELECT * FROM acad_calendar WHERE sem="2"';
+                $nextresult=mysqli_query($link,$qry2);
+                while ($row=mysqli_fetch_assoc($nextresult)) {
+                    echo
+                    '<tr> 
+          <td style="font-size: medium">' . $row['cal_id'] . '</td>
+          <td style="font-size: medium">' . $row['heading'] . '</td>
+          <td style="font-size: medium">' . $row['date_ug_pg'] . '</a></td>
+          <td style="font-size: medium">' . $row['date_else'] . '</a></td>
+          </tr>';
+                }
+
+                echo '</tbody>     
+</table>';
+                ?>
     </div>
     <?php include 'footer.php'; ?>
 
--- student.php
+++ student.php
@@ -9,50 +9,17 @@
   <link rel="stylesheet" href="stylesheets/style.css">
   <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
   <link rel="stylesheet" href="stylesheets/about.css">
-  <link rel="stylesheet" href="stylesheets/carousel.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <style>
-    .table-bordered tr th{
-      font-size: clamp(0.8rem,1.5vw,2.5rem);
+    .option {
+      display: flex;
+      justify-content: space-around;
+      position: absolute;
+      top: 80%;
+      width: 100%;
     }
-    tr td{
-      font-size: clamp(0.7rem,1.4vw,2.2rem);
-    }
-    #block {
-      height: 88%;
-      width: 70%;
-      margin: auto;
-      margin-top: 3%;
-      margin-bottom: 2.5%;
-      background-color: #ebe0d2;
-      box-shadow: 10px 10px grey;
-    }
-    @media screen and (max-width:800px){
-      #block{
-        width: 90%;
-      }
-      .table-bordered{
-        width:60%;
-      }
-    }
-    h1 {
-      margin-bottom: 1.2rem;
-      color: rgb(43, 43, 153);
-      text-shadow: 1px 1px 2px black;
-    }
-
-    .table-bordered>tbody>tr>td,
-    .table-bordered>tbody>tr>th,
-    .table-bordered>tfoot>tr>td,
-    .table-bordered>tfoot>tr>th,
-    .table-bordered>thead>tr>td,
-    .table-bordered>thead>tr>th {
-      border: 1px solid #555;
-    }
-
-    .table-bordered {
-      border: 3px solid #555;
-      width: 80%;
+    #click:checked~.option{
+      display: none;
     }
   </style>
 </head>
@@ -61,10 +28,10 @@
   <div class="navbar">
     <?php include 'topnav.php'; ?>
     <?php include 'mainnav.php'; ?>
-    <div class="about" style="top:50%;left:10%;transform:translate(-50%,-50%);">
+    <div class="about" style="top:50%;left:10%;transform:translate(-50%,-50%);width:fit-content">
       <h1>Enrolled Student</h1>
     </div>
-    <div style="display: flex; justify-content:space-around;position:absolute; top:80%; width:100%">
+    <div class="option">
       <div><a href="cse_students.php" style="background-color: blue;padding:20px;font-size: larger">CSE</a></div>
       <div><a href="ece_students.php" style="background-color: blue;padding:20px;font-size: larger">ECE</a></div>
     </div>
