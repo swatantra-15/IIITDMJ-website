3d7fc34c6105861e3eafa3410e14b867a6a5958c
swatantra-15 <87544866+swatantra-15@users.noreply.github.com>
2021-10-29 09:59:27 +0530

    Add files via upload

--- admin/update_Calquery.php
+++ admin/update_Calquery.php
@@ -7,8 +7,9 @@
         $heading = $_POST['heading'];
         $date_ug_pg = $_POST['date_ug_pg'];
         $date_else = $_POST['date_else'];
+
         
-        mysqli_query($link, "UPDATE `acad_calendar` SET `heading` = '$heading', `date_ug_pg` = '$date_ug_pg', `date_else` = '$date_else' WHERE `cal_id` = '$cal_id' AND `sem`='$sem' ") or die(mysqli_error());
+        mysqli_query($link, "UPDATE `acad_calendar` SET `heading` = '$heading', `date_ug_pg` = '$date_ug_pg', `date_else` = '$date_else' WHERE `cal_id` = '$cal_id'  ") or die(mysqli_error());
 
         header("location: calendar.php");
     }
